14504a0f7e7d27133f37ed152c5585156809ee0f
railroad12 <gb.browsing042@passmail.com>
2024-09-19 20:43:33 +0530

    Added windows notification
    Added code for windows notification such that on execution we will get
    notification

--- CreateTimeUpdateScheduledTask.ps1
+++ CreateTimeUpdateScheduledTask.ps1
@@ -1,7 +1,8 @@
 #will create scheduled task only for user who executes the scripts
 
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-Command `\"Set-TimeZone -Id 'India Standard Time'; w32tm /resync`\""
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-Command `"Set-TimeZone -Id 'India Standard Time'; w32tm /resync; [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null; $template = [Windows.UI.Notifications.ToastTemplateType]::ToastText01; $xml = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent($template); $text = $xml.GetElementsByTagName('text'); $text[0].AppendChild($xml.CreateTextNode('Time updated to IST successfully')); $toast = [Windows.UI.Notifications.ToastNotification]::new($xml); [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('TimeUpdateTask').Show($toast)`""
 #Action: Defines the task to set timezone to IST and resync time.
+#Notification Command: Adds a PowerShell command to display a Windows notification after updating the time to IST.
 
 $trigger = New-ScheduledTaskTrigger -AtLogOn
 #Trigger: Sets the task to run at user logon.
